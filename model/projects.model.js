791b9045444c1ed4507929fa5872c69e5a942ce6
Nyleesia Nicome <50892433+Nyleesia@users.noreply.github.com>
2020-01-30 15:32:53 -0500

    initial backend commit

---
+++ model/projects.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose"); 
+const Schema = mongoose.Schema;
+
+const ProjectsSchema = new Schema ({
+    projectID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }, 
+
+    name : {
+        type :  String, 
+        required : true,
+        trim: true
+    },
+
+    URL : {
+        type : String, 
+        required : true, 
+    },
+    
+    repo : {
+        type :  String, 
+        required : true,
+        trim: true, 
+        default:false
+    },
+
+    languages : {
+        type :  String, 
+        required : true,
+        trim: true, 
+        default: false
+    },
+
+    photo : {
+        type :  String, 
+        required : false,
+        trim: true,
+    },
+
+    addedOn : {
+        type : Date, 
+        required : true, 
+        default: Date.now
+    }
+})
+
+const Projects = mongoose.model("Projects", ProjectsSchema);
+
+module.exports = Projects;
